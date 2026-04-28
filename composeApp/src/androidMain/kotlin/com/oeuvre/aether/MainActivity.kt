e21e040bd5f8599f3eb8be2a1987cd0bd1dd2cc8
Pablo Campos <pablo.s.campos@outlook.com>
2026-04-28 17:48:44 -0500

    Update dark theme colors and refresh map styling.

--- composeApp/src/androidMain/kotlin/com/oeuvre/aether/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/oeuvre/aether/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.oeuvre.aether
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
@@ -9,7 +11,10 @@ import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.dark(Color.TRANSPARENT),
+            navigationBarStyle = SystemBarStyle.dark(Color.TRANSPARENT)
+        )
         super.onCreate(savedInstanceState)
 
         setContent {
